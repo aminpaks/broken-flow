bbfa0c89a7932d340cee1a589a867782ec33578c
Amin Paks <amin.pakseresht@hotmail.com>
2019-03-30 10:40:15 -0400

    Bugfix #7

--- src/feature-a/component-a.jsx
+++ src/feature-a/component-a.jsx
@@ -1,11 +1,14 @@
 import { getTest, neededForFix } from './util';
 
+export const Child = () => <span>OKAY</span>;
+
 export const ComponentA = () => {
     const updatedValueAC = neededForFix(2);
     const testValue = getTest(updatedValueAC, 'check');
     return (
         <div>
             Component A--{'FIX#6'}--FROM-BUGFIX-BRANCH--{testValue}--updated
+            <Child />
         </div>
     );
 };
